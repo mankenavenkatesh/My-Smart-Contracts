62c5e51d4e58af2988b9b6e23550290137e52f9e
Mankena <vmankena@BANL15013fa7c.local>
2018-06-19 22:01:24 +0530

    Quote Registry With Ownership

--- QuoteRegistry/contracts/QuoteRegistry.sol
+++ QuoteRegistry/contracts/QuoteRegistry.sol
@@ -1,20 +1,11 @@
 pragma solidity ^0.4.24;
 
 contract QuoteRegistry {
-
-    string public contractOwner;
+    
     address public owner;
 
     constructor() public {
         owner = msg.sender;
-    }
-
-    function changeContractOwner(string _newOwner) public {
-        contractOwner = _newOwner;
-    }
-
-    function getContractOwner() public view returns (string) {
-        return contractOwner;
     }
 }
 
