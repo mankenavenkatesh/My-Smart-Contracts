9749b8bee743ad75a6907515164f5ad6a31f57a9
mankenavenkatesh <vmankena@BANL15013fa7c.local>
2018-07-09 11:34:07 +0530

    Golf Challenge Gas Optimization

--- Solidity-golf-challenge/contracts/GolfChallege.sol
+++ Solidity-golf-challenge/contracts/GolfChallege.sol
@@ -1,20 +1,41 @@
-pragma solidity ^0.4.20;
+pragma solidity ^0.4.24;
 
 contract GolfChallenge {
-    address private owner;
     
-    constructor () {
-        owner = msg.sender;
-    }
-    
+    address private owner = msg.sender;
+    address private A;
+    uint private blockHeightLastExecution;
+
     modifier onlyOwner {
         require(msg.sender == owner);
         _;
     }
 
-    function deposit() onlyOwner {
-        
+    modifier onlyIfNext105Block() {
+        require(blockHeightLastExecution==0 || ((block.number) - (blockHeightLastExecution) >= 5));
+        _;
     }
 
+    function () onlyOwner external payable{
+    }
 
+    function setA(address _A) external onlyOwner {
+        A = _A;
+    }
+
+    function disburse() external onlyIfNext105Block {
+        uint X;
+        uint temp2 = blockHeightLastExecution;
+        if(temp2 == 0){
+            X = 5;
+        }else{
+            X = (temp2+1) % 100;
+        }
+        blockHeightLastExecution = block.number;
+        
+        if (X != 0) {
+            uint valueToDisburse =  X * ((address(this).balance))/(100);
+            A.transfer(valueToDisburse);
+        }
+    }
 }
